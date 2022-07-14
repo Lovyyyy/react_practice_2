d727792d4f4d07981c126d1272d2bf9d343a55d5
Lovyyyy <cys7773@naver.com>
2022-07-14 23:50:04 +0900

    -

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -3,8 +3,55 @@ const Title = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+const Container = styled.div``;
+
+const Header = styled.div``;
+const Coin = styled.h1``;
+const CoinsList = styled.li``;
+
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 const Coins = () => {
-  return <Title> COINS </Title>;
+  return (
+    <Container>
+      <Header>
+        <Title> COINS </Title>;
+      </Header>
+      <CoinsList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}> {coin.name} </Coin>
+        ))}
+      </CoinsList>
+    </Container>
+  );
 };
 
 export default Coins;
